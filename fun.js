7431a7a76993fc906dba5b0dc1ef03be560b1f4e
Ronald Pedid <ronaldpedid@live.com>
2019-03-26 13:16:44 -0500

    added some exercises

--- fun.js
+++ fun.js
@@ -25,11 +25,28 @@ for (let i = "#"; i.length <= hashes; i += "#") {
 
 //loop()
 
-for (var i = 1; i = 101; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+//fizzbuzz
+for (let i = 1; i < 101; i++) {
+    if (i % 15 == 0) {
+        console.log('FIZZBUZZ');
+    } else if (i % 3 == 0) {
+        console.log('FIZZ');
+    } else if (i % 5 == 0) {
+        console.log('Buzz')
+    } else {
+        console.log(i);
+    }
 }
-hello world
-Changes  
+//PRACTICE 
+
+const exercises = {
+    1: "assign 5 variables",
+    2: "write a for loop that counts to 255",
+    3: "write a conditional statement that turns a light on of off. (hint: use booleans)",
+    4: "write a conditional that adds or subtracts money/points based on a decisions made."
+}
+
+console.log(exercises[1]);
+console.log(exercises[2]);
+console.log(exercises[3]);
+console.log(exercises[4]);
