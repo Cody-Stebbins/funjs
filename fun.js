02096ff28e2d782b947f84bae01ac37590e95e7c
Cody Stebbins <c.stebb89@gmail.com>
2019-03-26 12:03:40 -0500

    added fun.js

---
+++ fun.js
@@ -0,0 +1,33 @@
+// loops with Ron
+
+// For loop with Ron
+
+/*let hashes = 17;
+for (let i = "#"; i.length <= hashes; i += "#") {
+    console.log(i)
+}*/
+
+// Fizz Buzz
+
+/*function loop() {
+
+    for (let i = 1; i <= 101; i++) {
+        if (i % 3 == 0) {
+            console.log("Fizz");
+        }
+        else if (i % 5 == 0) {
+            console.log("Buzz");
+        }
+        console.log(i);
+    }
+
+}*/
+
+//loop()
+
+for (var i = 1; i = 101; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
